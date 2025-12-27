8342ee9c991d68275e74292d5fff97571430dd03
sekiya-a <mingzhijiaguangu@gmail.com>
2025-12-27 11:40:32 +0900

    キービジュアルが完成しました。

--- js/main.js
+++ js/main.js
@@ -8,26 +8,26 @@ const switchViewport = () => {
   const viewportMeta = document.querySelector('meta[name="viewport"]');
 
   // 条件に基づいて適用するビューポートの設定を決定
-  const viewportContent = window.outerWidth > 380 ? 'width=device-width, initial-scale=1' : 'width=380';
+  const viewportContent = window.outerWidth > 380 ? "width=device-width, initial-scale=1" : "width=380";
 
   // ビューポート要素が存在しない場合はreturn
   if (!viewportMeta) return;
 
   // 現在のビューポートの設定が目的の設定と異なる場合にのみ、新しい設定を適用します。
-  if (viewportMeta.getAttribute('content') !== viewportContent) {
-    viewportMeta.setAttribute('content', viewportContent);
+  if (viewportMeta.getAttribute("content") !== viewportContent) {
+    viewportMeta.setAttribute("content", viewportContent);
   }
 };
 switchViewport();
-window.addEventListener('resize', switchViewport);
+window.addEventListener("resize", switchViewport);
 
 /**
  * ハンバーガーメニュー
  */
 const initializeHamburgerMenu = () => {
-  const menu = document.querySelector('.js-header-menu');
-  const openButton = document.querySelector('.js-header-menu-open-button');
-  const closeButton = document.querySelector('.js-header-menu-close-button');
+  const menu = document.querySelector(".js-header-menu");
+  const openButton = document.querySelector(".js-header-menu-open-button");
+  const closeButton = document.querySelector(".js-header-menu-close-button");
 
   // コンテンツ Opening Keyframe
   const contentsOpeningKeyframes = {
@@ -37,7 +37,7 @@ const initializeHamburgerMenu = () => {
   // コンテンツ Opening Option
   const contentsOpeningOptions = {
     duration: 200,
-    easing: 'ease-out',
+    easing: "ease-out",
   };
 
   // コンテンツ Closing Keyframe
@@ -48,7 +48,7 @@ const initializeHamburgerMenu = () => {
   // コンテンツ Closing Option
   const contentsClosingOptions = {
     duration: 200,
-    easing: 'ease-out',
+    easing: "ease-out",
   };
 
   // menuとopenButtonがページ内にない場合returnする
@@ -57,14 +57,14 @@ const initializeHamburgerMenu = () => {
   // メニューopenする関数
   const openMenu = () => {
     menu.showModal();
-    openButton.classList.add('is-open');
+    openButton.classList.add("is-open");
     menu.animate(contentsOpeningKeyframes, contentsOpeningOptions);
   };
 
   // メニューcloseする関数
   const closeMenu = () => {
     const closingAnim = menu.animate(contentsClosingKeyframes, contentsClosingOptions);
-    openButton.classList.remove('is-open');
+    openButton.classList.remove("is-open");
 
     // アニメーションの完了後
     closingAnim.onfinish = () => {
@@ -73,21 +73,50 @@ const initializeHamburgerMenu = () => {
   };
 
   // ボタンクリックでopen
-  openButton.addEventListener('click', () => {
+  openButton.addEventListener("click", () => {
     openMenu();
   });
 
   // クローズボタンクリックでclose
-  closeButton.addEventListener('click', () => {
+  closeButton.addEventListener("click", () => {
     closeMenu();
   });
 
   // Escapeキーを押すと非表示
-  document.addEventListener('keydown', function (event) {
-    if (event.key === 'Escape') {
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
       event.preventDefault();
       closeMenu();
     }
   });
 };
 initializeHamburgerMenu();
+
+/**
+ * トップページKVスライド
+ */
+const initializeTopMainVisual = () => {
+  document.addEventListener("DOMContentLoaded", function () {
+    // スライダーの存在を確認してから実行
+    const el = document.querySelector(".js-top-kv-splide");
+
+    // トップのスライドがページ内にない場合returnする
+    if (!el) return;
+
+    if (el) {
+      const splide = new Splide(el, {
+        type: "fade", // 左右に流さず、その場で切り替える
+        rewind: true, // 最後まで行ったら最初に戻る
+        speed: 3000, // 切り替わりの時間（3秒かけてじわ〜っと）
+        interval: 5000, // 次のスライドまでの待ち時間（5秒）
+        autoplay: true, // 自動再生
+        pauseOnHover: false, // マウスを乗せても止めない
+        arrows: false, // 左右の矢印を消す
+        pagination: false, // 下のドットを消す
+      });
+
+      splide.mount();
+    }
+  });
+};
+initializeTopMainVisual();
